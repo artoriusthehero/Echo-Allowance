9896e9e43d0562aadd780f61277f1033168f9502
artoriusthehero <84677862+artoriusthehero@users.noreply.github.com>
2021-10-11 21:34:18 -0400

    Update and rename README.md to needhelp.sh

--- README.md
+++
@@ -1,2 +0,0 @@
-# Echo-Allowance
-A rough game for tolerable gents
---
+++ needhelp.sh
@@ -0,0 +1,11 @@
+@ECHO OFF
+Hey guys, I got an idea for a cool reality tv show, it's where we hide and play tricks with each other without creating bot symbolism for how we're feeling. Instead of rubbing it into the girls faces, let's just bash our head into a wall
+
+If we're thinking of guidelines here, we know what to write: It's an echo batch container ACTION RPG ABOUT WINNING THE GAME THROUGH COMPUTER SCRIPT! Sort of. There's some deus ex machina, but I really just need help with the game engine, I'm really lost here and it can't be echo, it has to be sort of homebrew.
+
+I want options and dialogue like fallout without having to press about any mouses. A fully typable game. 
+I''legal code script error aside, i've been finding it difficult to rhyme to get your guy's attention.
+
+Need help!
+
+Can't program allowed!
